4a7a913052cb1e50056f13a72481a3b01ac142d9
agustin castro <agustincastrobaldini@gmail.com>
2021-07-28 17:38:30 -0300

    agregue-mi-nombre

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>NB Eventos</h1>
+        <h2>Agustin Castro Baldini</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
