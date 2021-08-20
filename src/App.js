b43f9b9759a1149a14fc966d41c26857152d0b9d
Agustin-CB <76254023+Agustin-CB@users.noreply.github.com>
2021-08-20 15:40:07 -0300

    agregando src actualizado

--- src/App.js
+++ src/App.js
@@ -3,51 +3,60 @@ import './App.css';
 import NavBar from './components/navBar.jsx';
 import { Component, useState } from 'react';
 import { render } from '@testing-library/react';
+import ItemCount from './components/itemCount';
 
-class MyComponent extends Component {
-  constructor() {
-    super()
-    console.log("contructor()")
-    this.state = {
-      name: "Jamon con queso"
-    }
+// class MyComponent extends Component {
+//   constructor() {
+//     super()
+//     console.log("contructor()")
+    
+//   }
+
+
+// componentDidMount() {
+//   console.log("componentDidMount")
+// }
+
+// componentWillUnmount () {
+//   console.log("componentWillUnmount")
+// }
+
+// render() {
+//   return(
+//     <button>Soy un boton</button>
+//   )
+// }
+
+// }
+
+
+// function App() {
+
+//   const [visible, setVisibility] = useState(true)
+
+//   const handleClick = () => {
+//     console.log("Hice click!")
+//   }
+
+
+//   return (
+//     <>
+//     <NavBar />
+//     {visible && <MyComponent />}
+//     <button onClick={handleClick}>Togable</button>
+
+//     </>
+//   );
+// }
+
+
+function App () {
+
+  const handleAdd = (cantidad) => {
+    console.log(cantidad)
   }
 
-
-componentDidMount() {
-  console.log("componentDidMount")
-}
-
-componentWillUnmount () {
-  console.log("componentWillUnmount")
-}
-
-render() {
-  return(
-    <button>Soy un boton {this.state.name}</button>
-  )
-}
-
-}
-
-
-function App() {
-
-  const [visible, setVisibility] = useState(true)
-
-  const handleClick = () => {
-    console.log("Hice click!")
-  }
-
-
-  return (
-    <>
-    <NavBar />
-    {visible && <MyComponent />}
-    <button onClick={handleClick}>Togable</button>
-
-    </>
-  );
+  return <ItemCount stock={5} initial={1} onAdd={handleAdd} />
 }
 
 export default App;
